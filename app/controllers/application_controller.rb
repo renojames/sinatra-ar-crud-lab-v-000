c7ecb29da3ba2cc83c7888aab57956c6d27b96fc
Reno Sorensen <renojames19@gmail.com>
2019-05-25 23:23:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    Article.create(params)
+    @article = Article.create(params)
+    binding.pry
+    redirect to "/articles/#{}"
   end
 
 end
