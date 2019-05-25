9844fee5956cec9c43c6556347e906ec84d54f8b
Reno Sorensen <renojames19@gmail.com>
2019-05-25 22:05:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,19 @@ class ApplicationController < Sinatra::Base
   get '/' do
   end
 
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
   get '/articles/new' do
     erb :new
   end
 
   post '/articles' do
     @article = Article.create(params)
-    binding.pry
+    @articles = Article.all
+    erb :index
   end
 
 end
